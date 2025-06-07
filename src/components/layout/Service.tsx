33c200fed91929ff3d3cb00753a6915ffb508c0f
bammyu <mbjun3630@naver.com>
2025-06-07 14:09:35 +0900

    fix: set default value for selected service state

--- src/components/layout/Service.tsx
+++ src/components/layout/Service.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { motion } from 'motion/react';
 import { ROUTE_CAT_PAGE, ROUTE_DOG_PAGE, ROUTE_HOME_PAGE } from '@/constants/route';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Service {
   label: string;
@@ -25,7 +25,11 @@ const services: readonly Service[] = [
 
 function Service() {
   const router = useRouter();
-  const [selectedService, setSelectedService] = useState(services[0]);
+  const pathname = usePathname();
+
+  const [selectedService, setSelectedService] = useState(
+    services.find((service) => service.route === pathname) || services[0],
+  );
 
   const handleServiceClick = (service: Service) => {
     setSelectedService(service);
@@ -42,7 +46,9 @@ function Service() {
             key={service.label}
             onClick={() => handleServiceClick(service)}
             className={`relative cursor-pointer rounded-2xl px-4 py-2 text-sm font-medium transition-colors duration-300 ${
-              isSelected ? 'font-semibold text-emerald-700 dark:text-emerald-400/80' : 'text-gray-500 hover:text-gray-800 dark:hover:text-gray-300'
+              isSelected
+                ? 'font-semibold text-emerald-700 dark:text-emerald-400/80'
+                : 'text-gray-500 hover:text-gray-800 dark:hover:text-gray-300'
             } `}
           >
             <span className="relative z-10">{service.label}</span>

e36f3e15124e8cf7b4d22a73da402e9dad92b4a0
bammyu <mbjun3630@naver.com>
2025-09-05 11:56:48 +0900

    feat: implement middleware for token refresh

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,30 @@
 import createFetchClient, { Middleware } from 'openapi-fetch';
 import createClient from 'openapi-react-query';
 import type { paths } from './schema';
+import { AuthInfoResponse } from './types';
+
+const _api = createFetchClient<paths>({ baseUrl: process.env.NEXT_PUBLIC_API_URL, credentials: 'include' });
 
 const middleware: Middleware = {
-  onRequest: ({ request, options }) => {},
-  onResponse: ({ request, response, options }) => {},
+  onResponse: async ({ request, response }) => {
+    if (!response.ok && response.status === 401) {
+      const refresh = await _api.GET('/api/v1/auth/refresh');
+      if (!refresh.error) {
+        return fetch(request);
+      }
+    }
+
+    if (response.ok && request.url === `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/info`) {
+      const body: AuthInfoResponse = await response.clone().json();
+
+      if (!body.isAuthenticated) {
+        const refresh = await _api.GET('/api/v1/auth/refresh');
+        if (!refresh.error) {
+          return fetch(request);
+        }
+      }
+    }
+  },
 };
 
 export const api = createFetchClient<paths>({ baseUrl: process.env.NEXT_PUBLIC_API_URL, credentials: 'include' });
--- src/api/types/index.ts
+++ src/api/types/index.ts
@@ -4,4 +4,5 @@ import type { components } from '@/api/schema';
 export type UserResponse = components['schemas']['UserResponse'];
 
 /** auth */
+export type AuthInfoResponse = components['schemas']['AuthInfoResponse'];
 export type AuthRegisterRequest = components['schemas']['AuthRegisterRequest'];

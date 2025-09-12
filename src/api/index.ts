106946df320685957c01f53eeb6d6479857c3546
bammyu <mbjun3630@naver.com>
2025-09-12 14:47:25 +0900

    feat: remove api middleware

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,33 +1,10 @@
-import createFetchClient, { Middleware } from 'openapi-fetch';
+import createFetchClient from 'openapi-fetch';
 import createClient from 'openapi-react-query';
 import type { paths } from './schema';
-import { AuthInfoResponse } from './types';
 
-const _api = createFetchClient<paths>({ baseUrl: process.env.NEXT_PUBLIC_API_URL, credentials: 'include' });
-
-const middleware: Middleware = {
-  onResponse: async ({ request, response }) => {
-    if (!response.ok && response.status === 401) {
-      const refresh = await _api.GET('/api/v1/auth/refresh');
-      if (!refresh.error) {
-        return fetch(request);
-      }
-    }
-
-    if (response.ok && request.url === `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/info`) {
-      const body: AuthInfoResponse = await response.clone().json();
-
-      if (!body.isAuthenticated) {
-        const refresh = await _api.GET('/api/v1/auth/refresh');
-        if (!refresh.error) {
-          return fetch(request);
-        }
-      }
-    }
-  },
-};
-
-export const api = createFetchClient<paths>({ baseUrl: process.env.NEXT_PUBLIC_API_URL, credentials: 'include' });
-api.use(middleware);
+export const api = createFetchClient<paths>({
+  baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  credentials: 'include',
+});
 
 export const $api = createClient(api);

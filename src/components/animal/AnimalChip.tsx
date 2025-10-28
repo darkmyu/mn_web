c27f3a12fece6abe0af24d2ae05ad746e27ad67b
bammyu <mbjun3630@naver.com>
2025-10-28 17:49:44 +0900

    feat: add default svg icon when animal thumbnail is missing

--- src/components/animal/AnimalChip.tsx
+++ src/components/animal/AnimalChip.tsx
@@ -1,4 +1,5 @@
 import { AnimalResponse } from '@/api/types';
+import { Cat, Dog } from 'lucide-react';
 import Image from 'next/image';
 
 interface Props {
@@ -8,9 +9,15 @@ interface Props {
 function AnimalChip({ animal }: Props) {
   return (
     <div className="flex cursor-pointer items-center gap-3 rounded-full bg-zinc-100 p-1.5 pr-4 dark:bg-zinc-800">
-      <div className="relative h-9 w-9">
+      <div className="relative flex h-9 w-9 items-center justify-center rounded-full bg-zinc-200 dark:bg-zinc-700">
         {animal.thumbnail && (
           <Image className="aspect-square rounded-full object-cover" src={animal.thumbnail} sizes="2vw" alt="" fill />
+        )}
+        {!animal.thumbnail && animal.breed.species === 'DOG' && (
+          <Dog className="h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+        )}
+        {!animal.thumbnail && animal.breed.species === 'CAT' && (
+          <Cat className="h-5 w-5 text-zinc-500 dark:text-zinc-400" />
         )}
       </div>
       <div>

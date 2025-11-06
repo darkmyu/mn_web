16051bb2d7982d321167f086307cca1b826e002f
bammyu <mbjun3630@naver.com>
2025-11-06 19:08:42 +0900

    feat: update sidebar header button to use back navigation on non-home pages

--- src/components/sidebar/SidebarHeader.tsx
+++ src/components/sidebar/SidebarHeader.tsx
@@ -1,18 +1,25 @@
 import { ROUTE_HOME_PAGE } from '@/constants/route';
-import { LucidePawPrint } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import { LucideArrowLeft, LucidePawPrint } from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
 
 function SidebarHeader() {
   const router = useRouter();
+  const pathname = usePathname();
+
+  const isHomePage = pathname === ROUTE_HOME_PAGE;
 
   const handleButtonClick = () => {
-    router.push(ROUTE_HOME_PAGE);
+    if (isHomePage) {
+      return router.push(ROUTE_HOME_PAGE);
+    }
+    router.back();
   };
 
   return (
     <div className="absolute top-0 left-0 flex h-16 w-full items-center justify-center">
       <button className="flex cursor-pointer items-center justify-center" onClick={handleButtonClick}>
-        <LucidePawPrint className="h-6 w-6 text-zinc-800 dark:text-zinc-200" />
+        {isHomePage && <LucidePawPrint className="h-6 w-6 text-zinc-800 dark:text-zinc-200" />}
+        {!isHomePage && <LucideArrowLeft className="h-6 w-6 text-zinc-800 dark:text-zinc-200" />}
       </button>
     </div>
   );

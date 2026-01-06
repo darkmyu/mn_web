925a344f7eb4b385e7d181f91d73ed770c831e07
bammyu <mbjun3630@naver.com>
2026-01-06 14:05:56 +0900

    feat: add profile animal popover

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig: NextConfig = {
       {
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        hostname: 'ssl.pstatic.net',
+      },
     ],
   },
   rewrites: async () => [
--- src/components/profile/ProfileAnimalList.tsx
+++ src/components/profile/ProfileAnimalList.tsx
@@ -3,9 +3,12 @@
 import { useProfileControllerAnimalsSuspense } from '@/api/profile';
 import { ROUTE_ANIMALS_WRITE_PAGE } from '@/constants/route';
 import { useAuthStore } from '@/stores/auth';
-import { Plus } from 'lucide-react';
+import dayjs from '@/utils/dayjs';
+import { formatAge } from '@/utils/formatters';
+import { LucideCake, LucideCat, LucideDog, LucideMars, LucideVenus, Plus } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
-import AnimalChip from '../animal/AnimalChip';
+import { Popover } from 'radix-ui';
 
 interface Props {
   username: string;
@@ -24,7 +27,68 @@ function ProfileAnimalList({ username }: Props) {
   return (
     <div className="flex items-stretch gap-4">
       {animals.items.map((animal) => (
-        <AnimalChip key={animal.id} animal={animal} />
+        <Popover.Root key={animal.id}>
+          <Popover.Trigger asChild>
+            <div className="flex cursor-pointer items-center gap-3 rounded-full bg-zinc-100 p-1.5 pr-4 dark:bg-zinc-800">
+              <div className="relative flex h-9 w-9 items-center justify-center rounded-full bg-zinc-200 dark:bg-zinc-700">
+                {animal.thumbnail && (
+                  <Image
+                    className="aspect-square rounded-full object-cover"
+                    src={animal.thumbnail}
+                    sizes="2vw"
+                    alt=""
+                    fill
+                  />
+                )}
+                {!animal.thumbnail && animal.breed.species === 'DOG' && (
+                  <LucideDog className="h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+                )}
+                {!animal.thumbnail && animal.breed.species === 'CAT' && (
+                  <LucideCat className="h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+                )}
+              </div>
+              <div>
+                <p className="text-sm font-semibold">{animal.name}</p>
+                <p className="text-xs text-zinc-600 dark:text-zinc-400">{animal.breed.name}</p>
+              </div>
+            </div>
+          </Popover.Trigger>
+          <Popover.Portal>
+            <Popover.Content
+              className="w-2xs rounded-lg bg-zinc-50 p-4 shadow-2xl/50 outline-none dark:bg-zinc-800"
+              sideOffset={12}
+              align="start"
+            >
+              <div className="flex flex-col gap-4">
+                <ul className="flex flex-col gap-1">
+                  <li className="flex items-center gap-2 text-zinc-500 dark:text-zinc-200">
+                    {animal.gender === 'MALE' && <LucideMars className="h-3.5 w-3.5 text-blue-400" />}
+                    {animal.gender === 'FEMALE' && <LucideVenus className="h-3.5 w-3.5 text-pink-400" />}
+                    <span className="text-sm font-medium">{animal.gender === 'MALE' ? '남아' : '여아'}</span>
+                  </li>
+                  <li className="flex items-center gap-2 text-zinc-500 dark:text-zinc-200">
+                    {animal.breed.species === 'DOG' && <LucideDog className="h-3.5 w-3.5" />}
+                    {animal.breed.species === 'CAT' && <LucideCat className="h-3.5 w-3.5" />}
+                    <span className="text-sm font-medium">{animal.breed.name}</span>
+                  </li>
+                  <li className="flex items-center gap-2 text-zinc-500 dark:text-zinc-200">
+                    <LucideCake className="h-3.5 w-3.5" />
+                    <span className="text-sm font-medium">{`${dayjs(animal.birthday).format('YYYY-MM-DD')} (${formatAge(animal.birthday)})`}</span>
+                  </li>
+                </ul>
+                {isOwner && (
+                  <Link
+                    href={`${ROUTE_ANIMALS_WRITE_PAGE}/${animal.id}`}
+                    className="cursor-pointer rounded-lg border-1 border-zinc-300 p-2.5 text-center text-sm font-semibold text-zinc-500 outline-none dark:border-zinc-200 dark:text-zinc-200"
+                  >
+                    반려동물 정보 수정
+                  </Link>
+                )}
+              </div>
+              <Popover.Arrow className="fill-zinc-50 dark:fill-zinc-800" width={16} height={8} />
+            </Popover.Content>
+          </Popover.Portal>
+        </Popover.Root>
       ))}
       {isOwner && (
         <Link href={ROUTE_ANIMALS_WRITE_PAGE}>

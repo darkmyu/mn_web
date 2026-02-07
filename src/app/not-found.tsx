661beaeeb5b543007a2ad194ab2459d7e55353c3
bammyu <mbjun3630@naver.com>
2026-02-07 23:27:00 +0900

    feat: 반응형 작업 3

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export default function NotFoundPage() {
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center gap-8 bg-zinc-50 px-4 text-center sm:gap-10 dark:bg-zinc-900">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-8 bg-zinc-50 px-4 text-center sm:gap-10 dark:bg-zinc-900">
       <Image className="aspect-square w-48 sm:w-72" src={NotFoundImage} alt="" width={300} height={300} priority />
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl font-bold sm:text-3xl">요청하신 페이지를 찾을 수 없어요.</h1>
@@ -11,6 +11,6 @@ export default function NotFoundPage() {
           주소가 잘못되었거나 더 이상 존재하지 않아요.
         </p>
       </div>
-    </main>
+    </div>
   );
 }

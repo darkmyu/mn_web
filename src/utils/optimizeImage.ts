6b80d2478b9e0f950dbf88d9054e704f541c6d1d
bammyu <mbjun3630@naver.com>
2026-03-06 14:52:59 +0900

    fix: 이미지 최적화 도메인 예외 처리

--- src/utils/optimizeImage.ts
+++ src/utils/optimizeImage.ts
@@ -1,5 +1,7 @@
 import type { ImageLoaderProps } from 'next/image';
 
+const ALLOWED_DOMAINS = ['image-dev.mongnyang.com', 'image.mongnyang.com'];
+
 export function optimizeImage({ src, width, quality }: ImageLoaderProps) {
   const params = [`width=${width}`, 'format=auto'];
 
@@ -9,7 +11,12 @@ export function optimizeImage({ src, width, quality }: ImageLoaderProps) {
 
   const url = new URL(src);
   const origin = url.origin;
+  const hostname = url.hostname;
   const pathname = url.pathname;
+
+  if (!ALLOWED_DOMAINS.includes(hostname)) {
+    return src;
+  }
 
   if (process.env.NODE_ENV === 'development') {
     return `${src}?${params.join('&')}`;
